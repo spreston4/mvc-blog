6920c7e84c20f5e4da0b51c5e6e3c33ceda4a289
spreston <sam.preston11@gmail.com>
2021-08-09 18:24:58 -0500

    Started API routes.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 
-// TODO: Replace placeholder function
-router.get('/', (req,res) => {
-    res.send('API PLACEHOLDER');
-});
+// Require User, Post, Comment
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./commentRoutes');
+
+// Route to Users, Posts, Comments
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
