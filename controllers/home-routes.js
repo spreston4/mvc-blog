66420ca8484512c263ab2b2a0a06f3bd38d48748
spreston <sam.preston11@gmail.com>
2021-08-14 11:44:15 -0500

    Renders all posts on homepage.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,19 +1,19 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 // Prevent non logged in users from viewing the homepage
 router.get('/', withAuth, async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
+    const postData = await Post.findAll({
+      // attributes: { exclude: ['password'] },
+      // order: [['name', 'ASC']],
     });
 
-    const users = userData.map((project) => project.get({ plain: true }));
+    const posts = postData.map((project) => project.get({ plain: true }));
 
     res.render('homepage', {
-      users,
+      posts,
       // Pass the logged in flag to the template
       logged_in: req.session.logged_in,
     });
