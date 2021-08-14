2b238fd9e2b1ad9c7a77dfd61f8d141b6cbf380d
spreston <sam.preston11@gmail.com>
2021-08-14 11:48:47 -0500

    logout functioning.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -58,7 +58,7 @@ router.post('/login', async (req, res) => {
 // User logout
 router.post('/logout', (req, res) => {
 
-    if(req.session.loggedIn) {
+    if(req.session.logged_in) {
         req.session.destroy(() => {
             res.status(204).end();
         });
